76c0bf6337082fba46f0d06029a08784feb2fe5f
Sandeep Arneja <sandeep45@gmail.com>
2016-03-16 15:13:51 -0400

    added bootstrap and committed the prod dist built and config files

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,10 @@
 console.log("this is app.js");
 
+require("jquery");
+require("bootstrap/dist/css/bootstrap.css");
+require("bootstrap/dist/css/bootstrap-theme.css");
+require("bootstrap");
+
 var React = require("react");
 var ReactDOM = require("react-dom");
 var TodoApp = require("./components/TodoApp.js");
